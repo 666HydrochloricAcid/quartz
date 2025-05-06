aa0c7671a7db13709001ee9dba4824e07c134750
Yeomsan <666hydrochloric@gmai.com>
2025-05-06 10:19:11 +0900

    Quartz sync: May 6, 2025, 10:19 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "666hydrochloricacid.github.io/quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
