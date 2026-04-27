23cc2d16cf543cfa038266f4400b72ea883a4902
Yeomsan <666hydrochloric@gmai.com>
2026-04-27 22:03:58 +0900

    config editted

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Cormorant Garamond",
-        body: "Cormorant Garamond",
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
       colors: {
